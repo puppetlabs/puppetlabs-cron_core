2164cbe478d0d95568832c9de45a66b3ead21301
Jorie Tappa <jorie@jorietappa.com>
2018-08-01 10:51:11 -0500

    Disable Security/YAMLLoad

--- spec/unit/provider/cron/crontab_spec.rb
+++ spec/unit/provider/cron/crontab_spec.rb
@@ -34,7 +34,7 @@ describe Puppet::Type.type(:cron).provider(:crontab) do
 
     ########################################################################
     # Simple input fixtures for testing.
-    samples = YAML.load(File.read(my_fixture('single_line.yaml')))
+    samples = YAML.load(File.read(my_fixture('single_line.yaml'))) # rubocop:disable Security/YAMLLoad
 
     samples.each do |name, data|
       it "should parse crontab line #{name} correctly" do
